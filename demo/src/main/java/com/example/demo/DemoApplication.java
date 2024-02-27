75eaf19a3896f5daa2a6a19b3a56051edcb0b4b4
ShoaibChuas1 <chaussi2018@gmail.com>
2024-02-27 15:36:35 +0530

    Dev1 Task completed

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.demo.service.Arithmetic;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ApplicationContext context= SpringApplication.run(DemoApplication.class, args);
+		
+		Arithmetic ar= context.getBean(Arithmetic.class);
+		
+		System.out.println("Sum :"+ar.sum(100, 200));
+		
+		System.out.println("Sub "+ar.sub(200, 100));
 	}
 
 }
--- demo/src/main/java/com/example/demo/service/Arithmetic.java
+++ demo/src/main/java/com/example/demo/service/Arithmetic.java
@@ -1,5 +1,16 @@
 package com.example.demo.service;
 
-public class Arithmetic {
+public class Arithmetic 
+{
+	public int sum(int x,int y)
+	{
+		return x+y;
+		
+	}
+	
+	public int sub(int x,int y)
+	{
+		return x-y;
+	}
 
 }
