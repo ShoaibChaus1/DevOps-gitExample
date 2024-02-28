ddf5ea1bbb8b9163dc60dc96e096d3b8b7598d5f
ShoaibChuas1 <chaussi2018@gmail.com>
2024-02-28 16:46:08 +0530

    Dev1 New Branche added

---
+++ demo/src/main/java/com/example/demo/reports/Reports.java
@@ -0,0 +1,14 @@
+package com.example.demo.reports;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Reports
+{
+	
+	public void weeklyReports()
+	{
+		System.out.println("Reports.weeklyReports()");
+	}
+
+}
